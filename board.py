64c3c915ac2fb53a7a0961eacff4ddc2b529e98c
Marco Milanta <marco.milanta@gmail.com>
2023-01-30 11:36:14 +0100

    ui intro

--- board.py
+++ board.py
@@ -21,11 +21,91 @@ plt.colorbar()
 plt.show()"""
 
 def main():
-    grid_width = 10
+    tk = Tk()
+
+
+    def gen_w(width,height,freq,thrs,scale):
+        board_wdw = Toplevel(tk)
+        board = Board(width,height)
+        board.generate_world(freq = freq, thrs=thrs)
+        board.render_world(tk = board_wdw,scale=scale)
+
+    width_lb = Label(text = "width:")
+    height_lb = Label(text = "height:")
+    freq_lb = Label(text = "freq:")
+    scale_lb = Label(text = "scale:")
+
+
+    width_tb = Text(tk, height=1,width = 10)
+    width_tb.insert("end-1c", 10)
+    height_tb = Text(tk, height=1,width = 10)
+    height_tb.insert("end-1c", 10)
+    freq_tb = Text(tk, height=1,width = 10)
+    freq_tb.insert("end-1c", 10)
+    scale_tb = Text(tk, height=1,width = 10)
+    scale_tb.insert("end-1c", 10)
+
+    see_lb = Label(text = "see:")
+    plains_lb = Label(text = "plains:")
+    forest_lb = Label(text = "forest:")
+    mountains_lb = Label(text = "mountains:")
+
+    see_LB = Label(text = "-inf")
+    see_UB = Text( height=1,width = 10)
+    see_UB.insert("end-1c", -0.2)
+    plains_LB = Label(text="↗")
+    plains_UB = Text( height=1,width = 10)
+    plains_UB.insert("end-1c", 0)
+    forest_LB = Label(text="↗")
+    forest_UB = Text( height=1,width = 10)
+    forest_UB.insert("end-1c", 0.3)
+    mountains_LB = Label(text="↗")
+    mountains_UB = Label(text = "+inf")
+
+    run_bt = Button(tk, text="generate", command=lambda:gen_w(int(width_tb.get("1.0",'end-1c')), 
+                                                              int(height_tb.get("1.0",'end-1c')), 
+                                                              float(freq_tb.get("1.0",'end-1c')), 
+                                                              scale = float(scale_tb.get("1.0",'end-1c')), 
+                                                              thrs = [float(see_UB.get("1.0",'end-1c')),
+                                                                      float(plains_UB.get("1.0",'end-1c')),
+                                                                      float(forest_UB.get("1.0",'end-1c'))]))
+
+
+    width_lb.grid(row=0,column=0)
+    height_lb.grid(row=1,column=0)
+    freq_lb.grid(row=2,column=0)
+    scale_lb.grid(row=3,column=0)
+    see_lb.grid(row=4,column=0)
+    plains_lb.grid(row=5,column=0)
+    forest_lb.grid(row=6,column=0)
+    mountains_lb.grid(row=7,column=0)
+
+    width_tb.grid(row=0,column=1)
+    height_tb.grid(row=1,column=1)
+    freq_tb.grid(row=2,column=1)
+    scale_tb.grid(row=3,column=1)
+
+    see_LB.grid(row=4,column=1)
+    plains_LB.grid(row=5,column=1)
+    forest_LB.grid(row=6,column=1)
+    mountains_LB.grid(row=7,column=1)
+
+    see_UB.grid(row=4,column=2)
+    plains_UB.grid(row=5,column=2)
+    forest_UB.grid(row=6,column=2)
+    mountains_UB.grid(row=7,column=2)
+
+    run_bt.grid(row=8,column=0)
+
+    
+
+    mainloop()
+    '''
+    grid_width = 5
     grid_height = 10
     board = Board(grid_width,grid_height)
     board.generate_world(freq = 4, thrs=[-.2,.0,.3])
-    board.render_world(scale=20)
+    board.render_world(scale=20)'''
 
 def discretize(val, th):
     out = 0
@@ -48,10 +128,9 @@ class Board():
             for col in range(self.width):
                 #self.mat[row][col] = random.choices([0,1,2,3], weights = [0,3,4,1],  k = 1)[0]
                 tmp = noise([freq*row/self.height,freq* col/self.width])
-                self.mat[row][col] = discretize(tmp,thrs)
+                self.mat[col][row] = discretize(tmp,thrs)
 
-    def render_world(self, scale = 10) -> None:
-        tk = Tk()
+    def render_world(self,tk, scale = 10) -> None:
         grid = HexagonalGrid(tk, scale = scale, grid_width=self.width, grid_height=self.height)
         grid.grid(row=0, column=0, padx=5, pady=5)
 
@@ -60,7 +139,7 @@ class Board():
             tk.quit()
         def save_to_pdf(canvas):
             canvas.postscript(file="tmp.ps", colormode='color')
-            somecommand = "gs -o output.pdf -sDEVICE=pdfwrite -dEPSCrop tmp.ps"
+            somecommand = "gswin64c -o output.pdf -sDEVICE=pdfwrite -dEPSCrop tmp.ps"
             process = subprocess.Popen(somecommand, shell=True)
             process.wait()
 
@@ -75,7 +154,7 @@ class Board():
                     "#706f6b"]
         for row in range(self.height):
             for col in range(self.width):
-                grid.setCell(row,col, fill=colors[self.mat[row][col]])
+                grid.setCell(col,row, fill=colors[self.mat[col][row]])
         tk.mainloop()
 
 
